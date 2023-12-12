57f58d28697f174cea7f18bfb3ce378f9b2f12ae
Nanji Lakan <86968487+Shaelle11@users.noreply.github.com>
2023-12-12 19:48:05 +0100

    made question tab, 3rd section responsive

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ closeBurger.addEventListener('click', function(){
 })
 
 }
-window.onload = function (){
+ function closeMenu (){
     let openBurger = document.getElementById('burger-open');
     let Menu = document.getElementById('the_menu');
     openBurger.addEventListener('click', function(){
@@ -16,5 +16,64 @@ window.onload = function (){
     })
     }
 
+    // document.addEventListener("DOMContentLoaded", function () {
+    //     const slideContainers = document.querySelectorAll(".question_links");
+    //     const prevButtons = document.querySelectorAll(".fa-chevron-left");
+    //     const nextButtons = document.querySelectorAll(".fa-chevron-right");
+      
+    //     let currentIndex = 0;
+      
+    //     function showSlide(index) {
+    //       const translateValue = -index * 100 + "%";
+    //       for (const container of slideContainers) {
+    //         container.style.transform = `translateX(${translateValue})`;
+    //       }
+    //     }
+      
+    //     function showNextSlide() {
+    //       if (currentIndex < slideContainers.length - 1) {
+    //         currentIndex++;
+    //         showSlide(currentIndex);
+    //       }
+    //     }
+      
+    //     function showPrevSlide() {
+    //       if (currentIndex > 0) {
+    //         currentIndex--;
+    //         showSlide(currentIndex);
+    //       }
+    //     }
+      
+    //     for (let i = 0; i < slideContainers.length; i++) {
+    //       prevButtons[i].addEventListener("click", showPrevSlide);
+    //       nextButtons[i].addEventListener("click", showNextSlide);
+    //     }
+    //   });
 
+window.onload = function () {
+        const slideContainers = document.querySelectorAll(".question_links");
+        const prevButton = document.getElementById("prevButton");
+        const nextButton = document.getElementById("nextButton");
+      
+        let currentIndex = 0;
 
+        function showSlide(index) {
+          const translateValue = -index * 100 + "%";
+          for (const container of slideContainers) {
+            container.style.transform = `translateX(${translateValue})`;
+          }
+        }
+      
+        function showNextSlide() {
+          currentIndex = (currentIndex + 1) % slideContainers.length;
+          showSlide(currentIndex);
+        }
+      
+        function showPrevSlide() {
+          currentIndex = (currentIndex - 1 + slideContainers.length) % slideContainers.length;
+          showSlide(currentIndex);
+        }
+      
+        prevButton.addEventListener("click", showPrevSlide);
+        nextButton.addEventListener("click", showNextSlide);
+      };
